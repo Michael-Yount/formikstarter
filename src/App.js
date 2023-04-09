11d7fc7e8aab113fdf2c580c77e90fa744565ba8
Michaeal Yount <cheshirekatsmile101@gmail.com>
2023-04-09 18:56:59 -0400

    fixed error message bug

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
       let errors = {};
       if (!values.email) errors.email = "field required";
       else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-        errors.email = '';
+        errors.email = 'Username should be an Email';
       }
       if (!values.password) errors.password = "field required";
     
